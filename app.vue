d69c3c45d8a2b61615981b6fcfda4143bc8cc3d4
reda <redaermich@gmail.com>
2022-09-09 15:07:12 +0100

    feat: add some catch error like duplicate symbol

--- app.vue
+++ app.vue
@@ -13,9 +13,6 @@ const keyEmit = (e: Event) => {
 	const targetValue = (e.target as HTMLButtonElement).value;
 	pressedKey.value = pressedKey.value.replaceAll('x', '*');
 	switch (targetValue) {
-		// case '+' || '-' || '/' || 'x':
-		// 	pressedKey.value = '0' + targetValue;
-		// 	break;
 		case 'DEL':
 			pressedKey.value = pressedKey.value.slice(0, -1);
 			break;
@@ -23,11 +20,38 @@ const keyEmit = (e: Event) => {
 			pressedKey.value = '';
 			break;
 		case 'Enter':
+			if (
+				pressedKey.value.endsWith('*') ||
+				pressedKey.value.endsWith('+') ||
+				pressedKey.value.endsWith('-') ||
+				pressedKey.value.endsWith('/') ||
+				pressedKey.value.endsWith('.')
+			) {
+				break;
+			}
 			pressedKey.value = eval(
 				/^0+\D/.test(pressedKey.value)
 					? pressedKey.value.replace(/^0+/, '0')
 					: pressedKey.value.replace(/^0+/, '')
 			).toString();
+			break;
+		case '+':
+		case '-':
+		case '/':
+		case 'x':
+		case '.':
+			const lastChar = pressedKey.value.slice(-1);
+			if (
+				lastChar === '+' ||
+				lastChar === '-' ||
+				lastChar === '/' ||
+				lastChar === '*' ||
+				lastChar === '.'
+			) {
+				pressedKey.value = pressedKey.value.slice(0, -1) + targetValue;
+			} else {
+				pressedKey.value += targetValue;
+			}
 			break;
 		default:
 			pressedKey.value += targetValue;
