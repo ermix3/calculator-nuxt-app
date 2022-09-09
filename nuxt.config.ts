d7c831dfaf2d92c451a640f77a44ecc7dce778a5
tenderking <gmu003@gmail.com>
2022-09-09 14:55:39 +0200

    config: try again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	ssr: false,
+	target: 'static',	
+  router: {
+    base: '/calculator-nuxt-app/'
+  },
 	modules: ["@nuxtjs/tailwindcss"],
 	buildModules: ["@nuxtjs/google-fonts"],
 	googleFonts: {
