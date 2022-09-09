642f03fc7eb26c62c9fac532ae53aebacce1135b
tenderking <gmu003@gmail.com>
2022-09-09 14:44:24 +0200

    config: another try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,12 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	ssr: false,
 	modules: ["@nuxtjs/tailwindcss"],
 	buildModules: ["@nuxtjs/google-fonts"],
 	googleFonts: {
 		families: {
-			"League Spartan": true,
-			// or:
-			// Lato: [100, 300],
-			// Raleway: {
-			//   wght: [100, 400],
-			//   ital: [100]
-			// },
+			"League Spartan": true,		
 		},
 	},
 	tailwindcss: {
@@ -22,5 +17,5 @@ export default defineNuxtConfig({
 		injectPosition: 0,
 		viewer: true,
 	},
-	target: 'static'
+
 });
