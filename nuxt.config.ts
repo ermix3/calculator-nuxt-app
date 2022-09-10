d4876c778e1caf2c77a182ef03bfce5b12074a4c
George <gmu003@gmail.com>
2022-09-10 22:12:02 +0200

    config: revert using vercel instead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,18 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	ssr: true,
-	router: {
-    base: '/calculator-nuxt-app/'
-	},
-	buildDir: 'calculator-nuxt-app',
-
-	modules: ["@nuxtjs/tailwindcss"],
-	buildModules: ["@nuxtjs/google-fonts"],
-	googleFonts: {
-		families: {
-			"League Spartan": true,		
-		},
-	},
-	tailwindcss: {
-		cssPath: "~/assets/css/tailwind.css",
-		configPath: "tailwind.config.js",
-		exposeConfig: false,	
-		injectPosition: 0,
-		viewer: true,
-	},
-
+  modules: ["@nuxtjs/tailwindcss"],
+  buildModules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "League Spartan": true,
+    },
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config.js",
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
 });
